58b2fa603844a082a2a1c491f0d9fed01461c0b2
GegaRazmadze <razmadzegega@gmail.com>
2024-07-25 18:55:38 +0400

    add port

--- NKMay/settingsprod.py
+++ NKMay/settingsprod.py
@@ -9,11 +9,15 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+
+
 import os
 from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,9 +87,10 @@ DATABASES = {
         'USER': 'maisiuser',
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': 'localhost',
-        'PORT': ''
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation

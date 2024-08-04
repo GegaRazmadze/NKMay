bb8c64743e8ff7253d419f851376ce6267bc4f28
GegaRazmadze <razmadzegega@gmail.com>
2024-08-04 17:40:32 +0400

    add

--- NKMay/settingsprod.py
+++ NKMay/settingsprod.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['hojei.com','.hojei.com','138.68.74.176','localhost']
+ALLOWED_HOSTS = ['138.68.74.176', 'localhost', '127.0.0.1','hojei.com','.hojei.com']
 
 
 # Application definition

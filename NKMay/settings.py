8f0582dd54963813b09f7e3fb4cd5b3a6cdd7c2d
GegaRazmadze <razmadzegega@gmail.com>
2024-08-05 19:28:32 +0400

    models py

--- NKMay/settings.py
+++ NKMay/settings.py
@@ -121,13 +121,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type

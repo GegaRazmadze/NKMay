96198834ed07f6750b48e55c6780d9df4ff029cb
GegaRazmadze <razmadzegega@gmail.com>
2024-08-06 23:43:07 +0400

    add domain

--- NKMay/settings.py
+++ NKMay/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['138.68.74.176', 'localhost', '127.0.0.1','hojei.com','.hojei.com']
+ALLOWED_HOSTS = ['138.68.74.176', 'localhost', '127.0.0.1','hojei.com','hojei.online']
 
 
 # Application definition

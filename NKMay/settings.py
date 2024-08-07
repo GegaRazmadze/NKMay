de1cb0857f6d7aa829544c815fad2cc7d9939dcd
GegaRazmadze <razmadzegega@gmail.com>
2024-08-07 12:37:59 +0400

    prepare for prod

--- NKMay/settings.py
+++ NKMay/settings.py
@@ -129,5 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
